a98885d1e1ff6ba0b8d935547879c8962c3702f4
tafita37 <118960434+tafita37@users.noreply.github.com>
2026-09-20 13:02:55 +0300

    reverse_storage

--- proposal/settings.py
+++ proposal/settings.py
@@ -149,15 +149,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
-    },
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
